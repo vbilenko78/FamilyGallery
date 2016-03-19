6afe9f5955665125a87519fa527d3c60871585bb
Vladimir Bilenko <vbilenko@yahoo.com>
2016-03-19 15:33:31 -0700

    Modified devise-invite

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,21 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
   before_filter :set_host_from_request
-
-  private
-
-  def set_host_from_request
-    ActionMailer::Base.default_url_options = { host: request.host_with_port }
-  end
 
   protected
   def authenticate_inviter!
     authenticate_admin!(:force => true)
   end
+
+  private
+  def set_host_from_request
+    ActionMailer::Base.default_url_options = { host: request.host_with_port }
+  end
+
 
   protected
   def configure_permitted_parameters
@@ -23,7 +24,7 @@ class ApplicationController < ActionController::Base
     # Override accepted parameters
     devise_parameter_sanitizer.for(:accept_invitation) do |u|
       u.permit(:first_name, :last_name, :phone, :password, :password_confirmation,
-	       :invitation_token)
+               :invitation_token)
     end
   end
 end
