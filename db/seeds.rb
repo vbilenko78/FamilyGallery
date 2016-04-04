34037695c7d3dcd9f77def67644a2b7f638d6b30
Vladimir Bilenko <vbilenko@yahoo.com>
2016-04-03 18:08:29 -0700

    modified seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-Member.create name: 'member-1', email: 'member-1@example.com', password: 'member-1@example.com', password_confirmation: 'member-1@example.com'
+Member.create! name: 'member-1', email: 'member-1@example.com', password: 'member-1@example.com', password_confirmation: 'member-1@example.com'
 
-Admin.create name: "admin-1", email: 'admin-1@example.com', password: 'admin-1@example.com', password_confirmation: 'admin-1@example.com'
+Admin.create! name: "admin-1", email: 'admin-1@example.com', password: 'admin-1@example.com', password_confirmation: 'admin-1@example.com'
 
-Gallery.create name: "First Gallery", owner: Admin.first, description:<<-EOS
+Gallery.create! name: "First Gallery", owner: Admin.first, description:<<-EOS
 This is my first test gallery!
-
 This is my work
 EOS
